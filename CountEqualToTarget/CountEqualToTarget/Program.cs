81d7137dfbd3bf5328b05850277cb48a7ab19473
2198187346@qq.com <2198187346@qq.com>
2026-06-11 21:50:45 +0800

    相同数字输出

---
+++ CountEqualToTarget/CountEqualToTarget/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CountEqualToTarget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=Convert.ToInt32(Console.ReadLine());
+            int[] arr=new int [n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i]= Convert.ToInt32(Console.ReadLine());
+            }
+            int m=Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] == m)
+                {
+                    Console.WriteLine(arr[i]);
+                    return;
+                }
+                
+            }
+        }
+    }
+}
