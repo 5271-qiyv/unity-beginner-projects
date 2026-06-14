d621d1c6eaedd765d60764bab20be07e5d5635ed
2198187346@qq.com <2198187346@qq.com>
2026-06-15 00:36:42 +0800

    计数问题

---
+++ DigitCount/DigitCount/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DigitCount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n=int.Parse(input[0]);
+            int x= int.Parse(input[1]);
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int temp = i;
+                while (temp > 0)
+                {
+                    
+                    if(temp% 10 == x)
+                    {
+                        count++;
+                    }
+                    temp /= 10;
+                }
+            }
+           
+            Console.WriteLine(count);
+        }
+    }
+}
