aa3b703799b905f48f2371ca1b68e772a7c97f14
2198187346@qq.com <2198187346@qq.com>
2026-06-15 13:54:38 +0800

    阶乘之和

---
+++ FactorialSum/FactorialSum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using System;
+namespace FactorialSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            BigInteger sum=0;
+            BigInteger factorial = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+                sum += factorial;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
