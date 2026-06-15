9c61f1d0549ef5521906c0d0b5998d913ca3ce70
2198187346@qq.com <2198187346@qq.com>
2026-06-15 13:30:11 +0800

    烟蒂交换

---
+++ SmokeSwap/SmokeSwap/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SmokeSwap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int k= int.Parse(Console.ReadLine());
+            int temp = n;
+            int total = n;
+            while (temp>k)
+                {
+                   total += temp / k;
+                temp = temp/k+temp%k;
+                }
+            Console.WriteLine(total);
+        }
+    }
+}
